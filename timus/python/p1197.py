2d941d71e950bf044388daf33b3bc3f7b2edc39c
Milena Araujo <m@milenaa.com>
2014-06-23 13:56:15 -0300

    rewriting timus' solutions

--- timus/python/p1197.py
+++ timus/python/p1197.py
@@ -1,6 +1,39 @@
 # Problem 1197. Lonesome Knight
 # http://acm.timus.ru/problem.aspx?space=1&num=1197
 
-n, m = map(int,raw_input().split())
+tests = int(raw_input())
+charmap = {"a": 1, "b": 2,"c": 3,"d": 4,"e": 5,"f": 6,"g": 7,"h": 8}
 
-print n*(m+1)
+def moviments(x, y):
+	moves = 0
+
+	if x + 2 <= 8:
+		if y + 1 <= 8:
+			moves += 1
+		if y - 1 > 0:
+			moves += 1
+
+	if x - 2 > 0:
+		if y + 1 <= 8:
+			moves += 1
+		if y - 1 > 0:
+			moves += 1
+
+	if y + 2 <= 8:
+		if x + 1 <= 8:
+			moves += 1
+		if x - 1 > 0:
+			moves += 1
+
+	if y - 2 > 0:
+		if x + 1 <= 8:
+			moves += 1
+		if x - 1 > 0:
+			moves += 1
+
+	return moves
+
+for x in xrange(0,tests):
+	place = raw_input()
+	print moviments(charmap[place[0]], int(place[1]))	
+
---
+++ timus/python/p1639.py
@@ -0,0 +1,4 @@
+# Problem 1639. Chocolate 2
+# http://acm.timus.ru/problem.aspx?space=1&num=1639
+
+tests = int(raw_input())
