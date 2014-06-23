eef7bcc12480955568abcee7e726c63e436f20b6
Milena Araujo <m@milenaa.com>
2014-06-23 11:18:36 -0300

    Correcting problem's url

--- timus/python/p2001.py
+++ timus/python/p2001.py
@@ -1,5 +1,5 @@
 # Problem 2001. Mathematicians and Berries
-# http://acm.timus.ru/problem.aspx?space=1&num=1209
+# http://acm.timus.ru/problem.aspx?space=1&num=2001
 
 a1, b1 = map(int,raw_input().split())
 a2, b2 = map(int,raw_input().split())
